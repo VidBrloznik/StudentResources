3d2c8ed2b338a41dfcebce9a7f672dfceda0c45d
VidBrloznik <121978367+VidBrloznik@users.noreply.github.com>
2025-09-08 15:47:39 +0200

    commiting

--- sr-backend/index.js
+++ sr-backend/index.js
@@ -38,8 +38,8 @@ app.use('/api', require('./routes/api'));
 app.use('/auth', require('./routes/auth'));
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
-app.listen(3000, () => {
-    console.log('Server is running on port: ' + 3000);
+app.listen(8170, () => {
+    console.log('Server is running on port: ' + 8170);
     
 });
 
